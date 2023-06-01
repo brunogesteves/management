d1f69e0d50017740e624f5907a1045eba193b2d9
Bruno <n3586@hotmail.com>
2023-06-01 10:30:55 -0300

    env

--- frontend/src/graphQl/GraphqlSetup.ts
+++ frontend/src/graphQl/GraphqlSetup.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
 
-const link = from([new HttpLink({ uri: process.env.REACT_APP_API_URL2 })]);
+const link = from([new HttpLink({ uri: process.env.REACT_APP_API_URL })]);
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
